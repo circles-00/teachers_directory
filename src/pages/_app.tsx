8126269fcec9c59868b84a0c88a01da6268b08c8
Nikola Krezeski <nkrezeski1@gmail.com>
2023-03-25 20:57:11 +0100

    ref: Change global font
    - Make dependencies versions exact

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,15 @@ import { type AppType } from 'next/app'
 import { api } from '~/utils/api'
 
 import '~/styles/globals.css'
+import { Public_Sans } from 'next/font/google'
+const publicSans = Public_Sans({ subsets: ['latin'] })
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <div className={publicSans.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default api.withTRPC(MyApp)
