559c915b2c303a2e523f3238e7f3cf36325c79fe
Nikola <nkrezeski@banzae.dev>
2023-05-21 03:01:06 +0200

    fix Build errors

--- src/server/api/routers/teachers.ts
+++ src/server/api/routers/teachers.ts
@@ -1,9 +1,11 @@
-import { createTRPCRouter, teacherProcedure } from "~/server/api/trpc";
-import { saveTeacherLocationPayload } from "~/server/api/logic/teachers/schema";
+import { createTRPCRouter, teacherProcedure } from '~/server/api/trpc'
+import { saveTeacherLocationPayload } from '~/server/api/logic/teachers/schema'
 import { TeacherService } from '../logic/teachers'
 
-export const  teachersRouter= createTRPCRouter({
-  saveTeacherLocation: teacherProcedure.input(saveTeacherLocationPayload).mutation(async ({ input }) => {
-    return TeacherService.saveTeacherLocation(input)
-  })
+export const teachersRouter = createTRPCRouter({
+  saveTeacherLocation: teacherProcedure
+    .input(saveTeacherLocationPayload)
+    .mutation(({ input }) => {
+      return TeacherService.saveTeacherLocation(input)
+    }),
 })
